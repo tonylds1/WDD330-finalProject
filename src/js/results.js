5052eede1c15d9940403d6cc431f636d6bcb42c9
McKay <wmsmckay@gmail.com>
2022-05-24 13:46:18 -0600

    Added titles and descriptions to pages

---
+++ src/js/results.js
@@ -0,0 +1,20 @@
+// import ElectronicPigeon from "./ePigeonCourierServices.js";
+import { flyInHeaderFooter, feedLocalStorage } from "./flyInNutsAndBolts.js";
+
+//put in the header & footer
+flyInHeaderFooter();
+
+//This is with the new folder name
+// let pigeon = new ElectronicPigeon();
+// let enteredSearch = "cars";
+// let bookId = "Zkp1AwAAQBAJ";
+// //can use this to increment to the next 40 if
+// //desired result is not found in the first 40
+// let searchBatch = 0;
+
+// // let pigeonResult = pigeon.bookListDeliveryPigeon40(enteredSearch, searchBatch);
+// // console.log(pigeonResult)
+
+// // let book = pigeon.pigeonBookDeliveryById(bookId);
+// // console.log(book);
+// // feedLocalStorage("read-shelf", book);
