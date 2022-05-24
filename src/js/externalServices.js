00cf7d477309faf10a13cf88003f6ce63a5620dc
JmmonJeremy <suc19002@byui.edu>
2022-05-23 20:34:44 -0700

    Started files

--- src/js/externalServices.js
+++
@@ -1,6 +0,0 @@
-// example book request with googleBooks API
-
-// https://www.googleapis.com/books/v1/volumes?q=search"<search>"
-
-// e.g. 
-// https://www.googleapis.com/books/v1/volumes?q=search"the+hungry+caterpillar"
---
+++ src/js/libraryPigeonDelivery.js
@@ -0,0 +1,2 @@
+//this module is used for most of the work done on the library.html page
+
