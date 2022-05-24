22996d94ad95d6943ed87747ea161a0cda454e58
JmmonJeremy <suc19002@byui.edu>
2022-05-24 08:29:18 -0700

    This branch feature is complete. I set it up for so that the buttons pull from the corresponding localStorage and will display when the code is added from the next card "User's booklist lists", which I will do next.

--- src/js/libraryDeliveryPigeon.js
+++ src/js/libraryDeliveryPigeon.js
@@ -3,28 +3,34 @@ import { grabLocalStorage, feedLocalStorage } from "./flyInNutsAndBolts.js"
 import ElectronicPigeon from "./ePigeonCourierServices.js"
 
 //create variable for ElectonicPigeon Module
-let wire = ElectronicPigeon();
+let wire = new ElectronicPigeon();
 
 //class for external use as a module
 export default class LibraryDeliveryBeak {
     constructor() {}
 
-    bookShelfFeeder(button) {
+    bookShelfFeeder(button, storageKey) {
         //create a listener even for a button click
         button.addEventListener("click", async () => {
         //create variable for the element to insert list into
         const listInsertionPoint = document.getElementById("my_book_lists");        
-        let readList = await grabLocalStorage("read_shelf");
+        let readList = await grabLocalStorage(storageKey);
 
-        readList = ["QwpEt9vUgBcC","LHfgyAEACAAJ","dI1AOAAACAAJ","t9czswEACAAJ","Zkp1AwAAQBAJ","yxfvwwEACAAJ","TtsRMQAACAAJ"]
+        readList = ["QwpEt9vUgBcC","LHfgyAEACAAJ","dI1AOAAACAAJ"]
 
         if (readList == null) {
+            //code to post this message onto the page
+            console.log("Nothing is here, try adding a book!");
             readList = [];
-            feedLocalStorage("read_shelf", readList);
+            feedLocalStorage(storageKey, readList);
         }
+        else{
+            feedLocalStorage(storageKey, readList);
+        }       
         let displayList = readList.forEach(bookId => {
             let myBook = wire.pigeonBookDeliveryById(bookId, false);
-            //code to post book onto template
+            //code to post book onto template will go here
+            console.log(myBook);
         })
     }); 
     }
