e5d4957479689ca2b399227795e79d48c2b05a5d
McKay <wmsmckay@gmail.com>
2022-05-25 11:39:33 -0600

    added basic display of search results

--- src/js/SearchResultsList.js
+++ src/js/SearchResultsList.js
@@ -1,5 +1,8 @@
 //Used for product-listing pages
-import { loadTemplate, renderListWithTemplate } from "./utils.js";
+import {
+  loadTemplate,
+  renderListWithTemplate
+} from "./utils.js";
 
 export default class SearchResults {
   constructor(searchTerm, dataSource, listElement) {
@@ -30,14 +33,40 @@ export default class SearchResults {
   }
 
   prepareTemplate(templateClone, book) {
-      console.log(book)
-    //   templateClone.querySelector("img").src = book.volumeInfo.imageLinks.smallThumbnail;
-    // templateClone.querySelector("a").href += product.Id;
-    // templateClone.querySelector("img").src = product.Images.PrimaryMedium;
-    // templateClone.querySelector("img").alt += product.Name;
-    // templateClone.querySelector(".card__brand").innerHTML = product.Brand.Name;
-    // templateClone.querySelector(".card__name").innerHTML =
-    //   product.NameWithoutBrand;
+    console.log(book)
+
+    // Add in the image
+    try {
+      templateClone.querySelector("img").src = book.volumeInfo.imageLinks.smallThumbnail;
+    } catch (e) {
+      templateClone.querySelector("img").src = "./images/bookCoverPlaceholder.gif";
+    }
+
+    templateClone.querySelector("img").alt = book.volumeInfo.title
+    templateClone.querySelector(".bookTitle").innerHTML = book.volumeInfo.title
+    let authors = templateClone.querySelector(".authors")
+    
+    // Add in the author
+    try {
+        book.volumeInfo.authors.forEach(author => {
+            authors.innerHTML +=  `${author}, `
+        });
+    } catch(e){
+        authors.innerHTML += "No Author Listed"
+    }
+    // remove the trailing space and comma on the last entry
+    authors.innerHTML = authors.innerHTML.slice(0, -2);
+
+    // Add in the publisher and the publish date
+    templateClone.querySelector(".publisher").innerHTML = book.volumeInfo.publisher
+    const options = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
+    let publishDate = new Date(book.volumeInfo.publishedDate)
+    templateClone.querySelector(".publishDate").innerHTML += publishDate.toLocaleDateString("en-US", options)
+
+    
+    // give functionality to each button
+    templateClone.querySelector(".addToShelfButtons")
+    
     return templateClone;
   }
 }
