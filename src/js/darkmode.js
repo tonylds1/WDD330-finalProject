fe30bbf7583cbe8d2658f35700b3a34283e8e72a
McKay <wmsmckay@gmail.com>
2022-05-31 09:53:34 -0600

    finished dark mode button text changes

--- src/js/darkmode.js
+++ src/js/darkmode.js
@@ -1,9 +1,11 @@
 //this module is used for most of the work done on the library.html page
-import { getLocalStorage, setLocalStorage } from "./utils.js";
+import {
+  getLocalStorage,
+  setLocalStorage
+} from "./utils.js";
 
 export default class DarkMode {
   constructor() {
-    // this.deviceColorSetting
     this.currentTheme = getLocalStorage("theme")
     this.prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)")
   }
@@ -11,51 +13,52 @@ export default class DarkMode {
   async init() {
     if (window.matchMedia("(prefers-color-scheme: dark)") && this.currentTheme == null) {
       this.currentTheme = "dark"
-    } else if (window.matchMedia("(prefers-color-scheme: light)").matches) {
-      this.deviceColorSetting = "light"
-    } else {
-      this.deviceColorSetting = "light";
-    }
+    } 
+    // else if (window.matchMedia("(prefers-color-scheme: light)") && this.currentTheme == null) {
+    //   this.currentTheme = "light"
+    // } else if (window.matchMedia("(prefers-color-scheme: no preference)") && this.currentTheme == null) {
+    //   this.currentTheme = "light"
+    // }
 
     // Set the page colors based on the current theme
     if (this.currentTheme == "dark") {
       this.darkMode()
-      //   document.body.classList.toggle("dark-mode");
     } else if (this.currentTheme == "light") {
       this.lightMode()
-      //   document.body.classList.toggle("light-mode");
     }
 
-    // give button functionality to toggle the dark mode at will
     await this.darkModeButton()
   }
 
-  toggleDarkMode() {
-    if (this.currentTheme == "light") {
-      this.currentTheme = "dark"
-      this.darkMode()
-    } else if (this.currentTheme == "dark") {
-      this.lightMode()
-      this.currentTheme = "light"
-    }
-
-    setLocalStorage("theme", this.currentTheme);
-  }
-
   async darkModeButton() {
-    let button = document.querySelector(".darkModeToggle");
-    button.addEventListener("click", () => {
-      this.toggleDarkMode()
-      //   console.log(this.currentColorSetting)
-    });
+    let buttonList = document.querySelectorAll(".darkModeToggle")
+    // console.log(buttons)
+    buttonList.forEach(button => {
+      if (this.currentTheme == "light"){
+        button.innerHTML = "Dark mode"
+      } else if (this.currentTheme == "dark") {
+        button.innerHTML = "Light mode"
+      }
+      button.addEventListener("click", () => {
+        if (this.currentTheme == "light") {
+          button.innerHTML = "Light mode"
+          this.currentTheme = "dark"
+          this.darkMode()
+        } else if (this.currentTheme == "dark") {
+          button.innerHTML = "Dark mode"
+          this.lightMode()
+          this.currentTheme = "light"
+        }        
+        setLocalStorage("theme", this.currentTheme);
+      })
+    })
+   
+    console.log(this.currentTheme)
   }
-
 
   darkMode() {
-    console.log("dark");
-    // let root = document.querySelector(":root");
-    // console.log()
-    // document.documentElement.style.setProperty("--tertiary-color", "lightblue");
+    // console.log("dark");
+    // document.querySelector(".darkModeToggle").innerHTML = "Light mode"
     document.documentElement.style.setProperty("--primary-color", "#1BD4BA");
     document.documentElement.style.setProperty("--secondary-color", "#000");
     document.documentElement.style.setProperty("--tertiary-color", "#bf8c00");
@@ -65,16 +68,11 @@ export default class DarkMode {
     document.documentElement.style.setProperty("--light-background", "#dadfe1");
     document.documentElement.style.setProperty("--dark-grey", "#fefefe");
 
-    // console.log(root.style.getPropertyValue("--white"))
-
-    // document.documentElement.style.setProperty("--tertiary-color", "black");
-
   }
 
   lightMode() {
-    console.log("light");
-    // let root = document.querySelector(":root");
-    // console.log(root.style.getPropertyValue("--tertiary-color"))
+    // console.log("light");
+    // document.querySelector(".darkModeToggle").innerHTML = "Dark mode"
     document.documentElement.style.setProperty("--primary-color", "#04AA6D");
     document.documentElement.style.setProperty("--secondary-color", "#cbf3f0");
     document.documentElement.style.setProperty("--tertiary-color", "#ffbf69");
@@ -83,8 +81,6 @@ export default class DarkMode {
     document.documentElement.style.setProperty("--text-color", "black");
     document.documentElement.style.setProperty("--light-background", "#fefefe");
     document.documentElement.style.setProperty("--dark-grey", "#000000");
-
-    // document.documentElement.style.setProperty("--tertiary-color", "white");
   }
 
 }
